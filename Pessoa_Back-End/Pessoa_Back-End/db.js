4015f25a97d22f842ca71b35f8c8d3668950fd4e
httpgabimarquez <gabrielamarquezzzz6@gmail.com>
2023-06-12 09:13:29 -0400

    alteração da rota

---
+++ Pessoa_Back-End/Pessoa_Back-End/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://znujhwvh:ebGyRpxPz7qMo0lavqS6tlhfqjArYEaA@silly.db.elephantsql.com/znujhwvh'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
