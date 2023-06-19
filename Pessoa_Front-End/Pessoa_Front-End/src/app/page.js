a63c2ed1b2e2703035250d025c02ea6b74628e57
Gabriela Marques <gabrielamarquezzzz6@gmail.com>
2023-06-19 11:32:08 -0400

    cor nova

--- Pessoa_Front-End/Pessoa_Front-End/src/app/page.js
+++ Pessoa_Front-End/Pessoa_Front-End/src/app/page.js
@@ -10,27 +10,31 @@ export default async function Home() {
   const produtos = await req.json();
 
   return (
-    <main> 
+    <main>
       <div class="sidebar">
-      <a><Link href="/cadastro" class="active" className='voltar'> Cadastrar seu produto </Link></a>
-  <a href="#news">Novos produtos</a>
-  <a href="#contact">Fale Conosco</a>
-  <a href="#about">Sobre</a>
-</div>
+        <a><Link href="/cadastro" class="active" className='voltar'> Cadastrar seu produto </Link></a>
+        <a href="#news">Novos produtos</a>
+        <a href="#contact">Fale Conosco</a>
+        <a href="#about">Sobre</a>
+      </div>
 
- <section id="lista">
-      {produtos.map(produto => (
-        <div key={produto.codigo}>
-          <section id="item1"> 
-          <b>{produto.titulo}</b>
-          <img src={produto.img} width={300} height={300}></img>
-          <p> {produto.data_de_cadastro}</p>
-          <p>{produto.preco}</p>
-          <p>{produto.descricao}</p>
-        <button>  <Link href={`/produto/${produto.codigo}`}>Ver mais...</Link></button> 
-          </section>
-        </div>
-      ))}
+      <section id="lista">
+        {produtos.map(produto => (
+          <div key={produto.codigo}>
+            <section id="item1">
+            <br/><br/>
+              <b>{produto.titulo}</b>
+              <br/><br/><br/>
+              <img src={produto.img} width={300} height={300}></img>
+              <p> {produto.data_de_cadastro}</p>
+              <p>{produto.preco}</p>
+              <p>{produto.descricao}</p>
+              <br/><br/>
+              <button>  <Link href={`/produto/${produto.codigo}`}>Ver mais</Link> </button>
+              <br/><br/><br/><br/>
+            </section>
+          </div>
+        ))}
       </section>
     </main>
   )
